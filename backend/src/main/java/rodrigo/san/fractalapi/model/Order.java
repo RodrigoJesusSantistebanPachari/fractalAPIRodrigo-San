7a06a266c81971a0b5c34486ad43e6b16459e51f
RodrigoJesusSantistebanPachari <58257135+RodrigoJesusSantistebanPachari@users.noreply.github.com>
2023-04-24 04:39:24 -0500

    Fractal: Generated id for product

--- backend/src/main/java/rodrigo/san/fractalapi/model/Order.java
+++ backend/src/main/java/rodrigo/san/fractalapi/model/Order.java
@@ -3,18 +3,20 @@ package rodrigo.san.fractalapi.model;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.MongoId;
 
-import java.util.Date;
-import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
 @Data
 @Document(collection = "orders")
-public class Order {
-    @Id
+public class Product {
+    @MongoId
     private String id;
-    private String OrderNr;
-    private Date date;
-    private int nProducts;
-    private List<Product> products;
-    private float finalPrice;
+    private String name;
+    private float unitPrice;
+    private int qty;
+    private float totalPrice;
 }
