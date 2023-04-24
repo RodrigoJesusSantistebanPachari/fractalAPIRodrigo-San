b1a68ab2b96471a02057ad3f74b7313bb970ddc7
RodrigoJesusSantistebanPachari <58257135+RodrigoJesusSantistebanPachari@users.noreply.github.com>
2023-04-24 04:38:20 -0500

    Fractal: Added Products Functionality

--- backend/src/main/java/rodrigo/san/fractalapi/controller/OrdersController.java
+++ backend/src/main/java/rodrigo/san/fractalapi/controller/OrdersController.java
@@ -2,6 +2,7 @@ package rodrigo.san.fractalapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import rodrigo.san.fractalapi.model.Order;
 import rodrigo.san.fractalapi.model.Product;
@@ -9,12 +10,18 @@ import rodrigo.san.fractalapi.repo.OrdersRepository;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
+
+import static java.util.UUID.randomUUID;
+
 
 @CrossOrigin
 @RestController
-@RequestMapping("")
+@RequestMapping("/orders")
 public class OrdersController {
     @Autowired
     private OrdersRepository ordersRepository;
@@ -28,11 +35,15 @@ public class OrdersController {
     @PostMapping("")
     Order create(@RequestBody Order order) {
 
+        System.out.println(order.getOrderNr());
+
         //Set current date
-        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-        Date date = new Date();
-        dateFormat.format(date);
+        ZoneId zoneId = ZoneId.of("America/Lima");
+        ZonedDateTime zdt = ZonedDateTime.now(zoneId);
+        Date date = Date.from(zdt.toInstant());
+
         order.setDate(date);
+
 
         //Count of selected products
         if (order.getProducts() != null) {
@@ -54,35 +65,45 @@ public class OrdersController {
     }
 
 
-    @PutMapping("{id}")
-    Order update(@PathVariable String id, @RequestBody Order order) {
-        Order orderFromDb = ordersRepository.findById(id).orElseThrow(RuntimeException::new);
+    @PutMapping("{id}/product")
+    Order createProduct(@PathVariable String id, @RequestBody Product product) {
 
+        System.out.println(product.getName());
+
+        Order orderFromDb = ordersRepository.findById(id).orElseThrow(RuntimeException::new);
         //Refresh to current date
-        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-        Date date = new Date();
-        dateFormat.format(date);
+
+        ZoneId zoneId = ZoneId.of("America/Lima");
+        ZonedDateTime zdt = ZonedDateTime.now(zoneId);
+        Date date = Date.from(zdt.toInstant());
+
+        orderFromDb.setDate(date);
+
+        //Set id of the product
+        product.setId(randomUUID().toString());
+        //Set Total Price of product
+        product.setTotalPrice(product.getQty() * product.getUnitPrice());
+        //Set new product
+        orderFromDb.addProduct(product);
+
+        return ordersRepository.save(orderFromDb);
+    }
+
+    @PutMapping("{id}")
+    Order updateProduct(@PathVariable String id, @RequestBody Order order) {
+        System.out.println("111111111111");
+        Order orderFromDb = ordersRepository.findById(id).orElseThrow(RuntimeException::new);
+        System.out.println("Pasó y encontró");
+        //Refresh to current date
+
+        ZoneId zoneId = ZoneId.of("America/Lima");
+        ZonedDateTime zdt = ZonedDateTime.now(zoneId);
+        Date date = Date.from(zdt.toInstant());
+
         orderFromDb.setDate(date);
 
         //Set new data
         orderFromDb.setOrderNr(order.getOrderNr());
-        orderFromDb.setProducts(order.getProducts());
-
-        //Count of selected products
-        if (order.getProducts() != null) {
-            orderFromDb.setNProducts(order.getProducts().size());
-        } else {
-            order.setNProducts(0);
-        }
-
-        //Complete the final price with products' prices
-        float finalPrice = 0;
-        if (order.getProducts() != null) {
-            for (Product product : order.getProducts()) {
-                finalPrice += product.getTotalPrice();
-            }
-        }
-        orderFromDb.setFinalPrice(finalPrice);
 
         return ordersRepository.save(orderFromDb);
     }
