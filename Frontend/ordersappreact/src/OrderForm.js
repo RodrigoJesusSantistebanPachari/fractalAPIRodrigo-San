0e88bff12a006c283b60765bd613363a269eaba8
RodrigoJesusSantistebanPachari <58257135+RodrigoJesusSantistebanPachari@users.noreply.github.com>
2023-04-24 17:52:18 -0500

    Fractal: Status added

--- Frontend/ordersappreact/src/OrderForm.js
+++ Frontend/ordersappreact/src/OrderForm.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { AvForm, AvField, AvInput, AvGroup } from "availity-reactstrap-validation";
-import { Button } from "reactstrap";
+import { Label, Button } from 'reactstrap';
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import { useRef } from "react";
 
@@ -11,8 +11,6 @@ const OrderForm = ({order, onSubmit}) => {
     let form = useRef();
 
     const _onSubmit = (values) => {
-        alert("Llegue");
-        alert(order);
         onSubmit(values);
         form.reset();
     }
@@ -32,15 +30,23 @@ const OrderForm = ({order, onSubmit}) => {
                 <div className="row justify-content-center">
                     <div className="col-md-6">
                         <div className="card border-primary">
-                            <div className="card-header bg-primary text-white">{order ? 'Edit': 'New'} Order</div>
+                            <div className="card-header bg-primary text-white">Add/Edit Order</div>
                             <div className="card-body">
                                 <AvForm ref={c => (form = c)} onValidSubmit={(_, values) => _onSubmit(values)}>
                                     <AvGroup className="mb-3">
+
+                                    <Label for="status">Status:</Label>
+                                    <AvField type="select" name="status" id="status">
+                                    <option value="0">Pending</option>
+                                    <option value="1">In Progress</option>
+                                    <option value="2">Completed</option>
+                                    </AvField>
+
                                         Order number:
                                         <AvField name="orderNr" required value={order ? order.OrderNr: ''}/>
                                     </AvGroup> 
                                     <div className="text-center">
-                                        <Button color="primary">{order ? 'Edit': 'New'} Order</Button>
+                                        <Button color="primary">Add/Edit Order</Button>
                                     </div>
                                 </AvForm>
                             </div>
@@ -53,3 +59,4 @@ const OrderForm = ({order, onSubmit}) => {
 }
 
 export default OrderForm;
+
