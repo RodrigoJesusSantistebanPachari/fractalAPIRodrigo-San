8be412660abb7d35ee9265bd1e8c19a9f0ce76d5
RodrigoJesusSantistebanPachari <58257135+RodrigoJesusSantistebanPachari@users.noreply.github.com>
2023-04-24 04:35:38 -0500

    Fractal: Better Style for Form

--- Frontend/ordersappreact/src/OrderForm.js
+++ Frontend/ordersappreact/src/OrderForm.js
@@ -1,16 +1,54 @@
 import React from "react";
 import { AvForm, AvField, AvInput, AvGroup } from "availity-reactstrap-validation";
+import { Button } from "reactstrap";
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import { useRef } from "react";
 
-const OrderForm = () => {
+
+
+const OrderForm = ({order, onSubmit}) => {
+
+    let form = useRef();
+
+    const _onSubmit = (values) => {
+        alert("Llegue");
+        alert(order);
+        onSubmit(values);
+        form.reset();
+    }
+
     return (
-        <>
-            <h3 className="mb-3">New Order</h3>
-            <AvForm>
-                <AvGroup className="mb-3">
-                    <p>Nombre:</p><AvField name = "nombre" required />
-                </AvGroup>
-            </AvForm>
-        </>
+        <div className="text-center">
+
+            <div className="banner d-flex justify-content-between">
+
+                <Link to="/" className="btn btn-secondary mr-auto">Return to index</Link>
+                    <h1 class="mx-auto">API For Fractal</h1>
+
+                <Link to="/my-orders"className="btn btn-success ml-auto">View my orders</Link>
+            </div>
+
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-6">
+                        <div className="card border-primary">
+                            <div className="card-header bg-primary text-white">{order ? 'Edit': 'New'} Order</div>
+                            <div className="card-body">
+                                <AvForm ref={c => (form = c)} onValidSubmit={(_, values) => _onSubmit(values)}>
+                                    <AvGroup className="mb-3">
+                                        Order number:
+                                        <AvField name="orderNr" required value={order ? order.OrderNr: ''}/>
+                                    </AvGroup> 
+                                    <div className="text-center">
+                                        <Button color="primary">{order ? 'Edit': 'New'} Order</Button>
+                                    </div>
+                                </AvForm>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
