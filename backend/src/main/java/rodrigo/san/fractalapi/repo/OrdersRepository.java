24b325813d2f20c6410449dfb5aea153a3ee6927
RodrigoJesusSantistebanPachari <58257135+RodrigoJesusSantistebanPachari@users.noreply.github.com>
2023-04-24 17:47:10 -0500

    Fractal: Added queries for status

--- backend/src/main/java/rodrigo/san/fractalapi/repo/OrdersRepository.java
+++ backend/src/main/java/rodrigo/san/fractalapi/repo/OrdersRepository.java
@@ -1,7 +1,19 @@
 package rodrigo.san.fractalapi.repo;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import rodrigo.san.fractalapi.model.Order;
 
+import java.util.List;
+
 public interface OrdersRepository extends MongoRepository<Order, String> {
+    @Query("{ 'status' : 0 }")
+    List<Order> findPendingOrders();
+
+    @Query("{ 'status' : 1 }")
+    List<Order> findInProgressOrders();
+
+    @Query("{ 'status' : 2 }")
+    List<Order> findCompletedOrders();
+
 }
