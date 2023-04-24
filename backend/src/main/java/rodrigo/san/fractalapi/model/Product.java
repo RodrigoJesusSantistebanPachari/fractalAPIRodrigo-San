00664dbb6a233577698276e70437258eea5c74f8
RodrigoJesusSantistebanPachari <58257135+RodrigoJesusSantistebanPachari@users.noreply.github.com>
2023-04-24 04:41:47 -0500

    Fractal: Automated id for Products added

--- backend/src/main/java/rodrigo/san/fractalapi/model/Product.java
+++ backend/src/main/java/rodrigo/san/fractalapi/model/Product.java
@@ -4,14 +4,16 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.MongoId;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
 @Data
+@Document(collection = "orders")
 public class Product {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @MongoId
     private String id;
     private String name;
     private float unitPrice;
